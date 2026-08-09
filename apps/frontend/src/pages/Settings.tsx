571261ce60bea1be3d74f8e0d0ec10c7307c8771
ahmdmazn658-ui <ahmdmazn658@gmail.com>
2026-08-09 17:34:50 +0300

    Update Settings.tsx

--- apps/frontend/src/pages/Settings.tsx
+++ apps/frontend/src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { moduleSettingGroups } from '../resources/settings';
 
 interface SettingField {
